a80fd13cd7c825254793ed2d474945968307057f
zuoz <zz121316128@gamil.com>
2021-11-28 12:28:03 +0800

    初次提交

---
+++ app/src/main/java/com/zuoz/zkeep/base/BaseVBAct.kt
@@ -0,0 +1,28 @@
+package com.zuoz.zkeep.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+
+
+abstract class BaseVBAct<B : ViewBinding> : AppCompatActivity() {
+    protected lateinit var mViewBinding: B
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mViewBinding = initViewBinding()
+        setContentView(mViewBinding.root)
+
+        initView()
+        initListener()
+        initData()
+    }
+
+
+    protected abstract fun initViewBinding(): B
+    protected abstract fun initView()
+    protected abstract fun initData()
+    protected abstract fun initListener()
+}
